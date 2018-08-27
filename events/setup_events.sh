f4180365d4d27325e9df18c8e292b0bd0637003c
ZigaSajovic <ziga.sajovic@gmail.com>
2018-08-27 16:15:06 +0200

    setup scripts

--- events/setup_events.sh
+++ events/setup_events.sh
@@ -3,14 +3,23 @@
 #make sure ran by sudo
 [ "$UID" -eq 0 ] || exec sudo "$0" "$(whoami)" "$@"
 
-for env in event_files/*;do 
+#handle acpi events
+for env in event_files/acpi_*;do 
   env_=/etc/acpi/events/${env##*/}
   cp $env $env_ 
   chown root:root $env_ 
   chmod 644 $env_ 
-
 done
 
+#handle udev events
+for env in event_files/acpi_*;do 
+  env_=/etc/acpi/events/${env##*/}
+  cp $env $env_ 
+  chown root:root $env_ 
+  chmod 644 $env_ 
+done
+
+#handle actions
 for ac in action_files/*;do
   ac_=/usr/bin/${ac##*/}
   cp $ac $ac_
