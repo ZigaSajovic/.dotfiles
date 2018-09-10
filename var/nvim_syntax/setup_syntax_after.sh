f39097b4cb577a508e2c17fb51e97e66f9e83405
ZigaSajovic <ziga.sajovic@gmail.com>
2018-09-10 22:05:35 +0200

    setup syntax after

--- var/nvim_syntax/setup_syntax_after.sh
+++ var/nvim_syntax/setup_syntax_after.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-syn=./function_call.vim
+syn_='function_call.vim'
 dir_=~/.config/nvim/after/syntax
 [[ ! -d $dir_ ]] && mkdir -p $dir_
 
@@ -8,9 +8,9 @@ append_if_must()
 {
   fle_=$dir_/$1
   [[ ! -f $fle_ ]] && touch $fle_
-  cat <(grep -v -x -f $syn_ $fle_) <(echo -e '"function call') $syn > $fle_
+  tmp=$(cat <(awk '{if (f==1) { r[$0] } else if (! ($0 in r)) { print $0 } } ' f=1 $syn_ f=2 $fle_) $syn_)
+  echo -e "$tmp" > $fle_
 }
-
 for f in $(sed '/^\s*$/d' ./lang_list);do
   append_if_must $f
 done
