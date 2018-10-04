2fc26c6bbf27c2c8eb746f6daac8ea7eb0f74c17
ZigaSajovic <ziga.sajovic@gmail.com>
2018-10-04 23:44:36 +0200

    setup scripts

--- setup_scripts.sh
+++ setup_scripts.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
 toAppend='export PATH="$HOME/.dotfiles/scripts:$PATH"'
-[[  -z $(grep $toAppend ~/.dotfiles/zsh/exports.zsh)  ]] && echo $toAppend >> ~/.dotfiles/zsh/exports.zsh && . ~/.dotfiles/zsh/exports.zsh
+[[  -z $(grep $toAppend ~/.dotfiles/zsh/exports.zsh)  ]] && echo $toAppend >> ~/.dotfiles/zsh/exports.zsh && source ~/.dotfiles/zsh/exports.zsh
 cd scripts
 sudo chmod u=rwx $(ls -p | grep -v /) 
