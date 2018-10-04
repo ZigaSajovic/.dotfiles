21a8311aca2d53d8445ce6b286a1e2009eece7c5
ZigaSajovic <ziga.sajovic@gmail.com>
2018-10-04 23:47:34 +0200

    config root

---
+++ var/brightness/config_root.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#make sure ran by sudo
+[ "$UID" -eq 0 ] || exec sudo "$0" "$(whoami)" "$@"
+
+echo "Setting script privileges"
+toAppend="$SUDO_USER ALL=(root) NOPASSWD: $HOME/.dotfiles/scripts/change_brightness"
+if [ -z "$(cat /etc/sudoers |grep $toAppend)" ]; then
+  echo $toAppend >> /etc/sudoers
+  . ~/.zshrc
+fi
+echo "Script now owned by root"
+chown root:root brightness.sh
+echo "Script only writable by root"
+chmod u+rwx brightness.sh
+echo "All users can execute the script"
+chmod go-w+rx brightness.sh
