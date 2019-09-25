77f1c3ae4017a469da5b444993333644aca15874
ZigaSajovic <ziga.sajovic@gmail.com>
2019-09-25 20:46:05 +0200

    new cuda

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,5 +1,5 @@
-export PATH=/usr/local/cuda-9.2/bin${PATH:+:${PATH}}
-export LD_LIBRARY_PATH=/usr/local/cuda-9.2/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export PATH=/usr/local/cuda-10.1/bin${PATH:+:${PATH}}
+export LD_LIBRARY_PATH=/usr/local/cuda-10.1/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
 export LD_LIBRARY_PATH=/usr/local/cuda/extras/CUPTI/lib64:$LD_LIBRARY_PATH
 export PATH="$PATH:$HOME/bin"
 VISUAL=nvim; export VISUAL
